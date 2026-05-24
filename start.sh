c2557b435d75a404519dbc232d8256bcdf1117ba
21210074-cpu <21210074@unima.ac.id>
2026-05-24 19:57:08 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-set -e
-php artisan route:clear
 php artisan config:clear
 php artisan migrate --force
 php artisan db:seed --force
 echo "Seeding done!"
-apache2-foreground
+exec apache2-foreground
